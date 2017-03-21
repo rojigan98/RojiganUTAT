5791c40336d4c6e9cc3b45faa721811fa7ca9ede
Rojigan <rojigan98@gmail.com>
2017-03-21 18:50:57 -0400

    Cropping works based off scale factor

--- test_pixel_color.py
+++ test_pixel_color.py
@@ -1,6 +1,9 @@
 import cv2
 WIDTH = 525 
 HEIGHT = 519
+SCALE_FACTOR = 1.1
+
+
   
 def find_limiting_p(image):
     minC = WIDTH - 1
@@ -50,15 +53,22 @@ dimm = max(parameters[1][0],parameters[1][1])
 new_bg = cv2.resize(bg, (int((int(b2) - int(b1))),int((int(a2) - int(a1)))), interpolation = cv2.INTER_LINEAR)
 
 
-final = cv2.resize(bg, (int(dimm * 1.2), int(dimm * 1.2)), interpolation = cv2.INTER_LINEAR) 
+final = cv2.resize(bg, (int(dimm * SCALE_FACTOR), int(dimm * SCALE_FACTOR)), interpolation = cv2.INTER_LINEAR) 
 
 
 
 new_bg = image[int(a1):int(a2), int(b1):int(b2)]   #yoffset:yoffset + zz, xoffset: xoffset + zzz
 
 
+new_new_bg = cv2.resize(bg, (int((int(b2) - int(b1)) * SCALE_FACTOR),int((int(a2) - int(a1)) * SCALE_FACTOR)), interpolation = cv2.INTER_LINEAR)
 
-final[final.shape[0]: 
+## horizontal, vertical
+
+hdm = int((int(b2) - int(b1)) * SCALE_FACTOR)
+vdm = int((int(a2) - int(a1)) * SCALE_FACTOR)
+
+hoff = (hdm - (int(b2) - int(b1))) // 2
+voff = (vdm - (int(a2) - int(a1)))// 2
 
 cv2.imshow("original", image)
 cv2.waitKey(0)
@@ -67,10 +77,13 @@ cv2.waitKey(0)
 # [0 0 0] is black
 # [255 255 255] is white 
 
+cv2.imshow("new_new", new_new_bg)
+cv2.waitKey(0)
 
+new_new_bg[voff:voff + int(a2) - int(a1), hoff:hoff + int(b2) - int(b1)] = new_bg 
 
-
-
+cv2.imshow("new_new", new_new_bg)
+cv2.waitKey(0)
 
 
 
