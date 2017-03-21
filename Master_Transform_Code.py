775cfa74a07062134c309e06f4aba3875f076141
Rojigan <rojigan98@gmail.com>
2017-03-21 19:06:12 -0400

    NOW I GOTTA CONVERT IT INTO A SQUARE

--- Master_Transform_Code.py
+++ Master_Transform_Code.py
@@ -17,8 +17,11 @@ import cv2
 import processImages
 import os 
 ROTATION_AMOUNT = 12
-LENGTH = 525 
-WIDTH = 519
+#LENGTH = 525 
+#WIDTH = 519
+WIDTH = 525 
+HEIGHT = 519
+SCALE_FACTOR = 1.1
 
 
 def compress_img(image, compression_amount):
@@ -28,19 +31,82 @@ def compress_img(image, compression_amount):
     new image to be 1/2 of what it was originally (everything else stays the same)'''
     
 
-    dim = (LENGTH//compression_amount, WIDTH)
+    dim = (WIDTH//compression_amount, HEIGHT)
     resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
     
     l_img = cv2.imread("Background.png", cv2.IMREAD_COLOR)
     s_img = resized
     
-    x_offset= LENGTH // 2 - s_img.shape[1]//2
-    y_offset= WIDTH // 2 - s_img.shape[0]//2
+    x_offset= WIDTH// 2 - s_img.shape[1]//2
+    y_offset= HEIGHT // 2 - s_img.shape[0]//2
     l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
-
-    return l_img
+    image = crop_image(l_img)
+    return image
     
     
+    
+    
+def crop_image(image):
+    '''Crops image to a constant scale factor, i.e the shape will take up 1/scale_factor X 100% of the final image'''
+    l_img = cv2.imread("Background.png", cv2.IMREAD_COLOR)
+    s_img = image
+    
+    parameters = (find_limiting_p(image))
+    
+    
+    bg = cv2.imread("Background.png", cv2.IMREAD_COLOR)
+    
+    a1 = parameters[0][1][0]
+    a2 = parameters[0][1][1]
+    
+    b1 = parameters[0][0][0]
+    b2 = parameters[0][0][1]
+    
+    dimm = max(parameters[1][0],parameters[1][1])
+    
+    
+    
+    new_bg = cv2.resize(bg, (int((int(b2) - int(b1))),int((int(a2) - int(a1)))), interpolation = cv2.INTER_LINEAR)
+    
+    
+    final = cv2.resize(bg, (int(dimm * SCALE_FACTOR), int(dimm * SCALE_FACTOR)), interpolation = cv2.INTER_LINEAR) 
+    
+    
+    
+    new_bg = image[int(a1):int(a2), int(b1):int(b2)]   #yoffset:yoffset + zz, xoffset: xoffset + zzz
+    
+    
+    new_new_bg = cv2.resize(bg, (int((int(b2) - int(b1)) * SCALE_FACTOR),int((int(a2) - int(a1)) * SCALE_FACTOR)), interpolation = cv2.INTER_LINEAR)
+    
+    ## horizontal, vertical
+    
+    hdm = int((int(b2) - int(b1)) * SCALE_FACTOR)
+    vdm = int((int(a2) - int(a1)) * SCALE_FACTOR)
+    
+    hoff = (hdm - (int(b2) - int(b1))) // 2
+    voff = (vdm - (int(a2) - int(a1)))// 2
+    new_new_bg[voff:voff + int(a2) - int(a1), hoff:hoff + int(b2) - int(b1)] = new_bg 
+    return new_new_bg 
+    
+def find_limiting_p(image):
+    minC = WIDTH - 1
+    minR = HEIGHT - 1
+    maxC = 0
+    maxR = 0
+    for i in range(0, HEIGHT):
+        for j in range(0, WIDTH):
+            if (image[i,j])[1] == 0:
+                continue
+            if i < minR:
+                minR = i
+            if j < minC:
+                minC = j
+            if j > maxC:
+                maxC = j
+            if i > maxR:
+                maxR = i
+                
+    return ((minC, maxC), (minR, maxR)), ((maxC - minC),(maxR -  minR)) 
     
 def rotate_img(image, rot_amount):
     '''image is simply the opencv image file
