b231e65d7f5a0765098925dd24f1c4dd33c690ee
Rojigan <rojigan98@gmail.com>
2017-04-12 10:23:09 -0400

    Added comments to Master_Transform_Code.py, Blur_images.py now dilates using a circular kernel instead of a rectangular one

--- Blur_images.py
+++ Blur_images.py
@@ -4,93 +4,108 @@ import Master_Transform_Code
 '''blurs 40 x 40 images '''
 #NEED TO UPDATE THIS FUNCTION
 #USE CIRCULAR DILATE
-#AND ADD SPACE ON THE SIDES SO THAT THE DILATE DOESNT GET CUT OFF 
+#AND ADD SPACE ON THE SIDES SO THAT THE DILATE DOESNT GET CUT OFF
 #DONT FORGET TO THRESHOLD IMAGES AT THE END
+#Dilate more - Davis
 '''input image is assumed to already be cropped and of proper size'''
 def addnoise(image):
 
-    #img_m = Master_Transform_Code.crop_image(img)
-    #img_m = cv2.resize(img_m,(42,42),0)
+
+
+    circular_Kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+
+    #High scale factor is to make sure none of the noise is cropped out
+    image = Master_Transform_Code.crop_image(image,1.5)
+
     #Blur is to get gray pixels
-    image = Master_Transform_Code.crop_image(image,1.2)
+    #See what happens if you use rectangular kernel here and circular kernel elsewhere
+    #GaussianBlur vs Blur? Blur replaces all the pixels of a kernel with the average value of that kernel
+    #This creates grey pixels near the edges of the shape, (Also in the shape, but these will be addressed later)
     blur = cv2.GaussianBlur(image,(5,5),0)
-    
-    #cv2.resize(img, (42,42), interpolation = cv2.INTER_AREA)
-    #blur = cv2.resize(blur, (img_m.shape[1],img_m.shape[0]), interpolation = cv2.INTER_AREA)
-    
+
+
+    #Creating the noise like this is satisfactory, for now at least
     noise = cv2.imread("Background.png", cv2.IMREAD_COLOR)
     noise = cv2.resize(noise, (int(blur.shape[1]) ,int (blur.shape[0])), interpolation = cv2.INTER_AREA)
-    
-    #noise = Master_Transform_Code.crop_image(noise)
-    
     make_random_white(noise)
-    
+
+    #blur = blur*noise, this line makes it so that all black pixels will stay black and only the gray pixels 
+    #will be affected, i.e. some of the gray pixels will become black due to the random noise, will be addressed in next
+    #section of code
+
     for i in range(0, noise.shape[0]):
-     for j in range(0, noise.shape[1]):
-        blur[i,j] = blur[i,j] * noise[i,j]
-    
-    
-    #NEED TO DILATE HERE
-    #new = blur + img_m
-    
-    new = blur + image
-    
-    
-    
+        for j in range(0, noise.shape[1]):
+            blur[i,j] = blur[i,j] * noise[i,j]
+
+
+
+    #Add the modified blur to the original image, grey + white = white, grey + black = grey, black + white = white
+    new = cv2.add(blur,image)
+    #new = blur + image <-- original line
+
+
+    #need to make all greys white
     threshold(new)
+
     cv2.imshow('before dilation',new)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     #if i use scale_factor of 1.1 then resize to 40 x 40 then the noise gets cut off, if i use scale factor of 1.2 then resize to 40 x 40 then the triangle will appear a bit smaller
-    new = Master_Transform_Code.crop_image(new,1.1)
+    new = Master_Transform_Code.crop_image(new,1.2)
     cv2.imwrite('before_dilation.png', new)
-    new = cv2.dilate(new,(3,3),iterations = 1)
+    new = cv2.dilate(new,circular_Kernel,iterations = 1)
     cv2.imwrite('after_dilation.png',new)
     return new
-    
+
 ''' Turns greys into blacks '''
 def threshold(image):
     for i in range(0, image.shape[0]):
         for j in range(0, image.shape[1]):
             if (image[i,j][0] != 0 and image[i,j][0] != 255):
-                for x in range(3): 
+                for x in range(3):
                     (image[i,j])[x] = 255
     return
-    
+
 '''puts random white spots on the background.png'''
 def make_random_white(image):
-    
+
     for i in range(0, image.shape[0]):
-        for j in range(0, image.shape[1]):            
+        for j in range(0, image.shape[1]):
             x = random.randint(0, 1)
             if (x == 0):
                 #image[i,j] = random.randint(0,1)
                 a = random.randint(0,1)
                 for k in range(3):
                     image[i,j][k] = a
-                
+
     return
 
 
 
 
 
-            
-                
-        
+
+
+
 if __name__ == '__main__':
-    
-    
-    image = cv2.imread("Equilateral_Triangle.png", cv2.IMREAD_COLOR)
-    image = Master_Transform_Code.crop_image(image,1.2)
-    image = cv2.resize(image,(40,40),0)
-    
-    cv2.imshow('image',image)
+
+
+    imgg = cv2.imread("Equilateral_Triangle.png", cv2.IMREAD_COLOR)
+    image = Master_Transform_Code.crop_image(imgg,1.2)
+    image = cv2.resize(image,(40,40),interpolation = cv2.INTER_AREA)
+
+    cv2.imshow('original image',image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
+
     image1 = addnoise(image)
-    
+
     cv2.imshow('after dilation',image1)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
+#check if the original imgg is preserved should be big with big borders
+    #normal imgg is preserved
+    cv2.imwrite("normal_image.png", imgg)
+
+    cv2.imwrite("blurred_image.png", image1)
--- Master_Transform_Code.py
+++ Master_Transform_Code.py
@@ -1,8 +1,8 @@
-'''Title: Master_Transform_Code.py  
+'''Title: Master_Transform_Code.py
 Author: Rojigan Gengatharan and the writer of the processImages.py code
-NOTES: Only works for images of 525 length and 519 width, these are the global 
+NOTES: Only works for images of 525 length and 519 width, these are the global
 constants. Requires the file names in the list shown at the beginning of the code
-to be present and requires a "Background.png" file which is a image of 
+to be present and requires a "Background.png" file which is a image of
 525 width and 519 height and colour is the same as the background of the images
 you are using. For example, A black background would mean Backgroud.png would
 have to be the colour black
@@ -11,91 +11,91 @@ Also requires processImages.py with the rotate_image function'''
 
 ##CAN UPLOAD MY FILES TO THE GOOGLE DRIVE
 #MAKE WIDE SHAPES
-##DONT FORGET TO THRESHOLD IMAGES AT THE END AND CONVERT TO GRAYSCALE
+##DONT FORGET TO THRESHOLD IMAGES AT THE END AND CONVERT TO GRAYSCALE, i.e. black and white
 ##PUT ALL IMAGES AS A BIG MATRIX IN ONE VECTOR
 ##DONT FORGET ABOUT DILATING IMAGES
 
 import numpy as np
 import cv2
 import processImages
-import os 
+import os
 ROTATION_AMOUNT = 12
-#LENGTH = 525 
+#LENGTH = 525
 #WIDTH = 519
-WIDTH = 525 
+WIDTH = 525
 HEIGHT = 519
 SCALE_FACTOR = 1.1
 DIMENSION = 40
-        
-                        
+
+
 def compress_img(image, compression_amount):
     '''image is simply the opencv image file
-    compression_amount is how much you want the file to be compressed, For 
-    example, a compression_amount of 2 indicates you want the horizontal size of the 
+    compression_amount is how much you want the file to be compressed, For
+    example, a compression_amount of 2 indicates you want the horizontal size of the
     new image to be 1/2 of what it was originally (everything else stays the same)'''
-    
+
 
     dim = (WIDTH//compression_amount, HEIGHT)
     resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-    
+
     l_img = cv2.imread("Background.png", cv2.IMREAD_COLOR)
     s_img = resized
-    
+
     x_offset= WIDTH// 2 - s_img.shape[1]//2
     y_offset= HEIGHT // 2 - s_img.shape[0]//2
     l_img[y_offset:y_offset+s_img.shape[0], x_offset:x_offset+s_img.shape[1]] = s_img
     image = crop_image(l_img, SCALE_FACTOR)
-    
-    res = cv2.resize(image, (WIDTH, HEIGHT), interpolation = cv2.INTER_LINEAR) 
+
+    res = cv2.resize(image, (WIDTH, HEIGHT), interpolation = cv2.INTER_LINEAR)
     return res
-    
-    
-    
-    
+
+
+
+
 def crop_image(image,sf):
-    '''Crops image to a constant scale factor, i.e the shape will take up 1/scale_factor X 100% of the final image'''
+    '''Returns a cropped image to a constant scale factor, i.e the shape will take up 1/scale_factor X 100% of the final image'''
     l_img = cv2.imread("Background.png", cv2.IMREAD_COLOR)
     s_img = image
-    
+
     parameters = (find_limiting_p(image))
-    
-    
+
+
     bg = cv2.imread("Background.png", cv2.IMREAD_COLOR)
-    
+
     a1 = parameters[0][1][0]
     a2 = parameters[0][1][1]
-    
+
     b1 = parameters[0][0][0]
     b2 = parameters[0][0][1]
-    
+
     dimm = max(parameters[1][0],parameters[1][1])
-    
+
     #next line for debugging purposes only
-    
-    
-    
+
+
+
     new_bg = cv2.resize(bg, (int((int(b2) - int(b1))),int((int(a2) - int(a1)))), interpolation = cv2.INTER_AREA)
-   
-    
+
+
     final_dim = int(dimm * sf)
-    
-    final = cv2.resize(bg, (final_dim, final_dim), interpolation = cv2.INTER_LINEAR) 
-    
-    
+
+    final = cv2.resize(bg, (final_dim, final_dim), interpolation = cv2.INTER_LINEAR)
+
+
     new_bg = image[int(a1):int(a2), int(b1):int(b2)]   #yoffset:yoffset + zz, xoffset: xoffset + zzz
-    
-    
+
+
     new_new_bg = cv2.resize(bg, (final_dim,final_dim), interpolation = cv2.INTER_LINEAR)
 
-    
+
     ## horizontal, vertical
 
-    
+
     hoff = (final_dim - (int(b2) - int(b1))) // 2
     voff = (final_dim - (int(a2) - int(a1)))// 2
-    new_new_bg[voff:voff + int(a2) - int(a1), hoff:hoff + int(b2) - int(b1)] = new_bg 
-    return new_new_bg 
-    
+    new_new_bg[voff:voff + int(a2) - int(a1), hoff:hoff + int(b2) - int(b1)] = new_bg
+    return new_new_bg
+
 def find_limiting_p(image):
     minC = image.shape[1] - 1
     minR = image.shape[0] - 1
@@ -113,59 +113,53 @@ def find_limiting_p(image):
                 maxC = j
             if i > maxR:
                 maxR = i
-                
-    return ((minC, maxC), (minR, maxR)), ((maxC - minC),(maxR -  minR)) 
-    
-    
+
+    return ((minC, maxC), (minR, maxR)), ((maxC - minC),(maxR -  minR))
+
+
 
 
 
 def rotate_img(image, rot_amount):
     '''image is simply the opencv image file
-    rotation_amount is how much you want the file to be rotated, For 
-    example, a rotation_amount of 12 means you want to rotate the image 12 
+    rotation_amount is how much you want the file to be rotated, For
+    example, a rotation_amount of 12 means you want to rotate the image 12
     degrees CCW'''
     new_img = processImages.rotate_image(image, rot_amount)
     return new_img
-    
-    
-    
+
+
+
 if __name__ == '__main__':
-    
-            
+
+
     list_of_file_names = ['Equilateral_Triangle.png', 'Isoceles_Right_Angle_Triangle.png']
-    
-    
+
+
     for i in range(len(list_of_file_names)):
-        
+
         img = cv2.imread(list_of_file_names[i], cv2.IMREAD_COLOR)
-        newpath = list_of_file_names[i][:-4]  
-        background = cv2.imread("Background.png", cv2.IMREAD_COLOR)  
+        newpath = list_of_file_names[i][:-4]
+        background = cv2.imread("Background.png", cv2.IMREAD_COLOR)
         if not os.path.exists(newpath):
             os.makedirs(newpath)
-        
+
         os.chdir(newpath)
-        cv2.imwrite("Background.png", background) 
+        cv2.imwrite("Background.png", background)
         # new_imgs = [new_img]
-        
+
         for j in range(1,31):
-            
+
             new_img = rotate_img(img, j*12)
             # new_imgs.extend(new_img)
-            
+
             for k in range(1, 5):
-                
+
                 new_new_img = compress_img(new_img, k)
                 new_new_img = cv2.resize(new_new_img, (DIMENSION,DIMENSION), interpolation = cv2.INTER_AREA)
-    
+
                 a = str(i) + 'th_shape' + str(j*12) + 'rot' + str(k) + 'squish_.png'
-                cv2.imwrite(a, new_new_img) 
-        
-    
+                cv2.imwrite(a, new_new_img)
+
+
         os.chdir('..')
-    
-    
-    
-        
-        
-        
